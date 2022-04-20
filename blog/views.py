aaa6dc5b80538e260b89448ead2dbe33cf0508fc
SolOFake <linkfazana2@gmail.com>
2022-04-20 05:17:15 +0300

    mail

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -33,3 +34,19 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):
+    # Получение статьи по идентификатору.
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if request.method == 'POST':
+        # Форма была отправлена на сохранение.
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Все поля формы прошли валидацию.
+            cd = form.cleaned_data
+            # ... Отправка электронной почты.
+        else:
+            form = EmailPostForm()
+            return render(request, 'blog/post/share.html',
+                          {'post': post, 'form': form})
